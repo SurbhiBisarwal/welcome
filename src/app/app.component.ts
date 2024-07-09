4199577c839425588641808c1ccf53f99b503bdc
surbhi <surbhibisarwal79@gmail.com>
2024-07-09 12:23:05 +0530

    ngModel and taking text input simple question

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'Surbhi Baudam hai';
-  courses:string[]=['Java','Angular','Android','Python'];
+  name: string = "Jack Smith";
+  email: string = " jack.smith@gmail.com";
+  number: string = "63129847";
 }
+
+//   submit() {
+//     let name = document.getElementById("name");
+//     let email = document.getElementById("email");
+//     let mobile = document.getElementById("mobile");
+//   }
+// }
