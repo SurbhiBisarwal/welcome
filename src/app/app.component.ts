fb2e7707610cb3e3b65c8066ce4ed84e83a839eb
surbhi <surbhibisarwal79@gmail.com>
2024-07-10 11:53:30 +0530

    Array of numbers (*ngFor)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,32 +6,6 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  teamAScore: number = 0;
-  teamBScore: number = 0;
-
-  point3(team): void {
-    if (team == "A") {
-      this.teamAScore = this.teamAScore + 3;
-    } else {
-      this.teamBScore = this.teamBScore + 3;
-    }
-  }
-  point2(team): void {
-    if (team == "A") {
-      this.teamAScore = this.teamAScore + 2;
-    } else {
-      this.teamBScore = this.teamBScore + 2;
-    }
-  }
-  freeThrow(team): void {
-    if (team == "A") {
-      this.teamAScore = this.teamAScore + 1;
-    } else {
-      this.teamBScore = this.teamBScore + 1;
-    }
-  }
-  reset(): void {
-    this.teamAScore = 0;
-    this.teamBScore = 0;
-  }
+  courses: string[] = ["Angular", "Java", "Android", "Python"];
+  num: number[] = [5, 8, 10, 11, 15, 3, 6];
 }
