ff2b68cbc58a55087afc773bf671b386383f3704
surbhi <surbhibisarwal79@gmail.com>
2024-07-12 14:19:19 +0530

    NG-A2#4 ascending And descending order odd and even  table should be display as per apply filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,9 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  num: number[] = [2, 3, 5, 7];
+  num: number[] = [21, 8, 10, 15, 7];
   n1: number = 0;
+  // filteredNum: number[] = [];
 
   add(n2): void {
     this.num.push(n2);
@@ -17,14 +18,30 @@ export class AppComponent {
     let n1: number[];
     console.log("i=" + i);
     n1 = this.num.splice(i, 1);
-    // this.n1 = n1[i];
   }
 
-  get sum(): number {
-    return this.num.reduce((acc, curr) => acc + curr, 0);
+  insertOrder(): void {
+    this.num = Array.from(this.num);
+    console.log(this.num);
   }
 
-  get evenCount(): number {
-    return this.num.reduce((acc, curr) => acc + (curr % 2 === 0 ? 1 : 0), 0);
+  ascending(): void {
+    this.num = Array.from(this.num).sort((a, b) => a - b);
+    console.log(this.num);
+  }
+
+  descending(): void {
+    this.num = Array.from(this.num).sort((a, b) => b - a);
+    console.log(this.num);
+  }
+
+  even(): void {
+    this.num = this.num.filter((n1) => n1 % 2 === 0);
+    console.log(this.num);
+  }
+
+  odd(): void {
+    this.num = this.num.filter((n1) => n1 % 2 !== 0);
+    console.log(this.num);
   }
 }
