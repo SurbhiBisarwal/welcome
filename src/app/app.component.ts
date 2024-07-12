a2b8f4f9b4b0ad29d5849a0c453599ee027bd3c4
surbhi <surbhibisarwal79@gmail.com>
2024-07-12 20:39:26 +0530

    NG-A2#5 Product Table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,42 +6,45 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  num: number[] = [21, 8, 10, 15, 7];
-  n1: number = 0;
-  // filteredNum: number[] = [];
+  products = [
+    { id: "P104", brand: "Pepsi", price: 4, qtySold: 22 },
+    { id: "C124", brand: "Coke", price: 4, qtySold: 26 },
+    { id: "M155", brand: "Maggie", price: 6, qtySold: 10 },
+    { id: "DM241", brand: "Cadburys", price: 10, qtySold: 15 },
+    { id: "5S118", brand: "5 Star", price: 8, qtySold: 8 },
+  ];
+  newProduct = {
+    id: "",
+    brand: "",
+    price: 0,
+    qtySold: 0,
+  };
 
-  add(n2): void {
-    this.num.push(n2);
+  getTotalQuantitySold() {
+    return this.products.reduce((total, product) => total + product.qtySold, 0);
   }
 
-  delete(i: number): void {
-    let n1: number[];
-    console.log("i=" + i);
-    n1 = this.num.splice(i, 1);
+  getTotalSalesValue() {
+    return this.products.reduce(
+      (total, product) => total + product.price * product.qtySold,
+      0
+    );
+  }
+  resetQuantity(product) {
+    product.qtySold = 0;
   }
 
-  insertOrder(): void {
-    this.num = Array.from(this.num);
-    console.log(this.num);
+  incrementQuantity(product) {
+    product.qtySold += 1;
   }
-
-  ascending(): void {
-    this.num = Array.from(this.num).sort((a, b) => a - b);
-    console.log(this.num);
-  }
-
-  descending(): void {
-    this.num = Array.from(this.num).sort((a, b) => b - a);
-    console.log(this.num);
-  }
-
-  even(): void {
-    this.num = this.num.filter((n1) => n1 % 2 === 0);
-    console.log(this.num);
-  }
-
-  odd(): void {
-    this.num = this.num.filter((n1) => n1 % 2 !== 0);
-    console.log(this.num);
+  add() {
+    if (
+      this.newProduct.id &&
+      this.newProduct.brand &&
+      this.newProduct.price > 0
+    ) {
+      this.products.push({ ...this.newProduct });
+      this.newProduct = { id: "", brand: "", price: 0, qtySold: 0 };
+    }
   }
 }
