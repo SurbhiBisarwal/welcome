f44e1141cf4ac882ed34b246c0aeea054602bec2
surbhi <surbhibisarwal79@gmail.com>
2024-07-14 15:08:04 +0530

    ngIf    NG-A3#1    v1||v2      v1&&v2        -----------------get message1(): string {     return this.v1 || this.v2 ? "Good" : "";   }

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,33 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
+// export class AppComponent {
+//   check: boolean = true;
+//   makeTrue(V1, V2): void {
+//     console.log("In function MakeTrue");
+//     this.check = true;
+//   }
+//   makeFalse(V1, V2): void {
+//     console.log("In function MakeFalse");
+//     this.check = false;
+//   }
+// }
 export class AppComponent {
-  check: boolean = true;
+  v1: boolean = true;
+  v2: boolean = true;
 
-  makeTrue(): void {
-    console.log("In function MakeTrue");
-    this.check = true;
+  setV1(value: boolean): void {
+    this.v1 = value;
   }
-  makeFalse(): void {
-    console.log("In function MakeFalse");
-    this.check = false;
+
+  setV2(value: boolean): void {
+    this.v2 = value;
+  }
+
+  // get message1(): string {
+  //   return this.v1 || this.v2 ? "Good" : "";
+  // }
+  get message(): string {
+    return this.v1 && this.v2 ? "Excellent" : "";
   }
 }
