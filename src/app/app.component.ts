5fba4e2a3bcca305cba9529e3d7ea39c938a4595
surbhi <surbhibisarwal79@gmail.com>
2024-07-15 14:00:57 +0530

    x is zero, positive, negative NG-A3#2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,33 +5,12 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-// export class AppComponent {
-//   check: boolean = true;
-//   makeTrue(V1, V2): void {
-//     console.log("In function MakeTrue");
-//     this.check = true;
-//   }
-//   makeFalse(V1, V2): void {
-//     console.log("In function MakeFalse");
-//     this.check = false;
-//   }
-// }
 export class AppComponent {
-  v1: boolean = true;
-  v2: boolean = true;
-
-  setV1(value: boolean): void {
-    this.v1 = value;
+  x: number = 0;
+  add(): void {
+    this.x++;
   }
-
-  setV2(value: boolean): void {
-    this.v2 = value;
-  }
-
-  // get message1(): string {
-  //   return this.v1 || this.v2 ? "Good" : "";
-  // }
-  get message(): string {
-    return this.v1 && this.v2 ? "Excellent" : "";
+  sub(): void {
+    this.x--;
   }
 }
