a8ccbe1babf27809ca8570f9a8722611c49aa356
surbhi <surbhibisarwal79@gmail.com>
2024-07-16 23:01:24 +0530

    NG-A3#4A2 Watch the demo of the Code App - Part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,32 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
+  showMainPage: boolean = true;
+  showAB: boolean = false;
+  show19: boolean = false;
+  showMN: boolean = false;
   codes: string[] = [];
   newCode: string[] = [];
+  codeType: string = "";
+
+  codePattern(section: string) {
+    this.showMainPage = false;
+    this.showAB = false;
+    this.show19 = false;
+    this.showMN = false;
+
+    if (section === "ab") {
+      this.codeType = "AB123";
+      this.showAB = true;
+    } else if (section === "09") {
+      this.codeType = "0-9";
+      this.show19 = true;
+    } else if (section === "mn") {
+      this.codeType = "MN012";
+      this.showMN = true;
+    }
+  }
+
   addCode() {
     const codeString = this.newCode.join("");
     if (codeString) {
@@ -26,5 +50,9 @@ export class AppComponent {
 
   clearCode(): void {
     this.newCode = [];
+    this.showMainPage = true;
+    this.showAB = false;
+    this.show19 = false;
+    this.showMN = false;
   }
 }
