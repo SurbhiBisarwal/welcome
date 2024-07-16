f751035b7935a170f1c0ace14949de04c3bab95f
surbhi <surbhibisarwal79@gmail.com>
2024-07-16 11:48:44 +0530

    Array of Bob and Steve NG-A3#3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,5 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  resultOfA: string = "pass";
-  resultOfB: string = "pass";
-  setA(value: string): void {
-    this.resultOfA = value;
-    console.log(this.resultOfA);
-  }
-  setB(value: string): void {
-    this.resultOfB = value;
-    console.log(this.resultOfB);
-  }
+  array: string[] = ["Bob", "Bob", "Steve", "Bob", "Steve"];
 }
