d6e58fd42441798a99924e6408fd45d7ce968c67
surbhi <surbhibisarwal79@gmail.com>
2024-07-25 21:39:17 +0530

    Checkbox and Radio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,10 @@
-import { Book } from "./Books";
 import { Component } from "@angular/core";
-import { booksdb } from "./Books";
-
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  books: Book[] = booksdb.slice(); // Create a copy of booksdb for return function
-
-  reservedBook: string[] = [];
-
-  reserveBook(book: Book) {
-    const index = this.books.findIndex((data) => data == book);
-    this.books.splice(index, 1);
-    this.reservedBook.push(book.name);
-    console.log(this.reservedBook);
-  }
-
-  returnReservedBook(item: string) {
-    const index = this.reservedBook.findIndex((name) => name === item);
-    console.log("Return book name:", item);
-    console.log("Reserved book index:", index);
-    if (index !== -1) {
-      this.reservedBook.splice(index, 1);
-      const returnedBook = booksdb.find((book) => book.name === item);
-      if (returnedBook) {
-        this.books.push(returnedBook);
-
-        console.log("Books after return:", this.books);
-      }
-    }
-  }
+  working: boolean;
+  gender: string = "";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
-import { BooksComponent } from "./books/books.component";
 
 @NgModule({
-  declarations: [AppComponent, BooksComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
