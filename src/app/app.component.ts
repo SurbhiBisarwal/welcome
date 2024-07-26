fa5ac1cc716bb4f1a6c00b97df5c87767497d35c
surbhi <surbhibisarwal79@gmail.com>
2024-07-26 12:03:00 +0530

    NG-A4#2 checkbox  Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  working: boolean;
-  gender: string = "";
+  working = false;
+  studying = false;
+  companyName = "";
+  designation = "";
+  collegeName = "";
+  course = "";
+
+  onStatusChange(event: any) {
+    const status = event.target.value;
+    if (status === "working") {
+      this.working = true;
+      this.studying = false;
+    } else if (status === "studying") {
+      this.working = false;
+      this.studying = true;
+    }
+  }
 }
