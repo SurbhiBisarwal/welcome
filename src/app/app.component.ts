81a91f1654a3b0addc39e19d4ceedaa4add78b46
surbhi <surbhibisarwal79@gmail.com>
2024-08-02 13:20:46 +0530

    NG-A5#2 Events on dropdown Events on checkbox and radio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  length: number = 0;
-
-  event(value: string) {
-    this.length = value.length;
+  courses: string[] = ["Angular", "Python", "Java", "Spring Boot"];
+  event(s: string) {
+    console.log(s);
   }
 }
