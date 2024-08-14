face9a6e91efdcb9135fd62afeca7dd19b33c015
surbhi <surbhibisarwal79@gmail.com>
2024-08-14 20:48:57 +0530

    REDIRECT     Create Home along with Hello and Bye

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HelloComponent } from "./hello/hello.component";
 import { ByeComponent } from "./bye/bye.component";
+import { HomeComponent } from "./home/home.component";
 const routes: Routes = [
   {
     path: "hello",
@@ -10,6 +11,15 @@ const routes: Routes = [
   {
     path: "bye",
     component: ByeComponent,
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: " ",
+    redirectTo: "home",
+    pathMatch: "full",
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { FormsModule } from "@angular/forms";
 import { HelloComponent } from "./hello/hello.component";
 import { ByeComponent } from "./bye/bye.component";
 import { Router } from "@angular/router";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HelloComponent, ByeComponent],
+  declarations: [AppComponent, HelloComponent, ByeComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
